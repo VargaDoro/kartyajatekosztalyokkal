8521bf64620b692ba3c6e56cdc47ac71a98f7116
Vajkyy <vajki2014@gmail.com>
2025-09-10 15:10:02 +0200

    feltolt(), kirak() megirasa

--- src/huszonegybuvesztrukk/HuszonegyBuvesztrukk.java
+++ src/huszonegybuvesztrukk/HuszonegyBuvesztrukk.java
@@ -2,8 +2,37 @@ package huszonegybuvesztrukk;
 
 public class HuszonegyBuvesztrukk {
 
-    public static void main(String[] args) {
-        
+    static String[] pakli = new String[21];
+
+    public static void feltolt() {
+        String[] szinek = {"P", "T", "Z", "M"};
+        String[] ertek = {"Ász", "kir", "Fel", "X", "IX", "VIII"};
+        int index = 0;
+        for (int i = 0; i < szinek.length && index < pakli.length; i++) {
+            for (int j = 0; j < ertek.length && index < pakli.length; j++) {
+                pakli[index] = szinek[i] + "_" + ertek[j];
+                index++;
+            }
+        }
     }
-    
+
+    public static void kirak() {
+        int sorokSzama = 7;
+        int oszlopokSzama = 3;
+
+        for (int sor = 0; sor < sorokSzama; sor++) {
+            for (int oszlop = 0; oszlop < oszlopokSzama; oszlop++) {
+                int index = sor + oszlop * sorokSzama;
+                if (index < pakli.length && pakli[index] != null) {
+                    System.out.print(pakli[index] + "\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        feltolt();
+        kirak();
+    }
 }
